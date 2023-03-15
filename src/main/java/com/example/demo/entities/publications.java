1523e71088b2da972d5ee2771610e0b9de0faf12
Jeison Ortiz <ortizjeison4@gmail.com>
2023-03-14 22:07:49 -0500

    feat(entities): Se agrega la entidad publicacion

---
+++ src/main/java/com/example/demo/entities/publications.java
@@ -0,0 +1,73 @@
+package com.example.demo.entities;
+
+import org.hibernate.dialect.ColumnAliasExtractor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "publications",uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class publications {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "title", nullable = false)
+	private String title;
+	
+	@Column(name = "description", nullable = false)
+	private String description;
+	
+	public publications(Long id, String title, String description, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.content = content;
+	}
+
+	public publications() {
+		super();
+	}
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
