9f2501566d9e4edec16e541d9364326d4849f0c2
Jeison Ortiz <ortizjeison4@gmail.com>
2023-04-11 19:57:17 -0500

    feat(Configuration | controller | dto): - Se agrega been a sprinn security y se hace el login

--- src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,19 +31,34 @@ public class SecurityConfig {
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests()
+		http.csrf().disable()
+		.authorizeHttpRequests()
             .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+            .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+            
             .anyRequest().authenticated()
             .and()
             .httpBasic();   
 		return http.build();
     }
 	
+	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 	auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
 	}
  
+	@Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//	    return authenticationManagerBean();
+//	}
+//	
+
 //	@Bean
 //	public UserDetailsService userDetailsService() {
 //		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
